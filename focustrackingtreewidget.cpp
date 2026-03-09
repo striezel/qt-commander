032dd4cb77019bd6fe0fda72eaaae54ae6ffd09c
Dirk Stolle <striezel-dev@web.de>
2026-03-09 13:41:22 +0100

    remove debug output in FocusTrackingTreeWidget
    It's been in use long enough to know that it works even without
    the explicit feedback from the debug output.

--- focustrackingtreewidget.cpp
+++ focustrackingtreewidget.cpp
@@ -1,7 +1,5 @@
 #include "focustrackingtreewidget.h"
 
-#include <QDebug>
-
 FocusTrackingTreeWidget::FocusTrackingTreeWidget(QWidget *parent)
     : QTreeWidget(parent)
 {
@@ -15,9 +13,7 @@ const QDateTime &FocusTrackingTreeWidget::focusTime() const
 
 void FocusTrackingTreeWidget::focusInEvent(QFocusEvent *event)
 {
-    qDebug() << "focusInEvent (TreeView): " << this->objectName();
     latestFocusTime = QDateTime::currentDateTimeUtc();
-    qDebug() << "UTC: " << latestFocusTime;
 
     // Call inherited method to do the actual work.
     this->QTreeWidget::focusInEvent(event);
