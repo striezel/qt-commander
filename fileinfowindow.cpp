2f31802743b58c336e6f69a7c72075aa5f011ad4
Dirk Stolle <striezel-dev@web.de>
2026-03-12 10:44:41 +0100

    add information about owner and group to file info window

--- fileinfowindow.cpp
+++ fileinfowindow.cpp
@@ -33,6 +33,34 @@ void FileInfoWindow::loadInformation(const QString &filePath)
 
     ui->lblFileName->setText(info.fileName());
 
+    // On systems where owner id and group id are not supported or files do not
+    // have owners or groups, the id will be -2, cast to uint, according to Qt
+    // documentation. So let's check that here.
+    constexpr uint NoId = static_cast<uint>(-2);
+    QString owner = info.owner();
+    if (owner.isEmpty())
+    {
+        owner = "<unbekannt>";
+    }
+    const uint owner_id = info.ownerId();
+    if (owner_id != NoId)
+    {
+        ui->lblOwnerValue->setText(owner + " (" + QString::number(owner_id) + ")");
+    }
+    else
+    {
+        ui->lblOwnerValue->setText(info.owner());
+    }
+    const uint group_id = info.groupId();
+    if (group_id != NoId)
+    {
+        ui->lblGroupValue->setText(info.group() + " (" + QString::number(group_id) + ")");
+    }
+    else
+    {
+        ui->lblGroupValue->setText(info.group());
+    }
+
     const QFileDevice::Permissions permissions = info.permissions();
 
     ui->cbUserRead->setChecked(permissions.testFlag(QFileDevice::Permission::ReadUser));
