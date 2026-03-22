279011311972e921e18f61cb6d2f10355beeadfb
Dirk Stolle <striezel-dev@web.de>
2026-03-22 04:32:10 +0100

    ci: add GHA workflow to build Debian packages

---
+++ ci/02-create-package.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#  create-package.sh - script to create *.deb package from latest Git commit
+#
+#  Copyright (C) 2019, 2022, 2025, 2026  Dirk Stolle
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Gets the latest tag.
+TAG="${CI_COMMIT_TAG:-$(git describe | cut -d '-' -f 1)}"
+# Version is the same as the tag, just without the leading 'v'.
+VERSION=$(echo $TAG | cut -c '2-')
+ARCH=$(dpkg --print-architecture)
+COMMIT_HASH=$(git rev-parse HEAD)
+
+echo "Info: Tag is $TAG."
+echo "Info: Version is $VERSION."
+echo "Info: Architecture is $ARCH."
+echo "Info: Commit hash is $COMMIT_HASH."
+
+if [ -z ${GITHUB_ACTIONS+x} ]
+then
+  # Download from GitLab (default).
+  wget -O qt-commander_${VERSION}.orig.tar.bz2 https://gitlab.com/striezel/qt-commander/-/archive/${COMMIT_HASH}/qt-commander-${COMMIT_HASH}.tar.bz2
+  tar -x --bzip2 -f qt-commander_${VERSION}.orig.tar.bz2
+  if [[ $? -ne 0 ]]
+  then
+    echo "ERROR: Archive extraction failed!"
+    exit 1
+  fi
+else
+  # Download from GitHub.
+  wget -O qt-commander_${VERSION}.orig.tar.gz https://github.com/striezel/qt-commander/archive/${COMMIT_HASH}.tar.gz
+  tar -x --gzip -f qt-commander_${VERSION}.orig.tar.gz
+  if [[ $? -ne 0 ]]
+  then
+    echo "ERROR: Archive extraction failed!"
+    exit 1
+  fi
+fi
+
+mv qt-commander-${COMMIT_HASH} qt-commander-${VERSION}
+if [[ $? -ne 0 ]]
+then
+  echo "ERROR: Could not move extracted files!"
+  exit 1
+fi
+
+cd qt-commander-${VERSION}
+if [[ $? -ne 0 ]]
+then
+  echo "ERROR: Could not change to extracted directory"
+  exit 1
+fi
+
+# build the package
+debuild -uc -us
+if [[ $? -ne 0 ]]
+then
+  echo "ERROR: Package build failed!"
+  exit 1
+fi
+
+cd ..
+RELEASE=$(lsb_release -cs)
+mv qt-commander_*.deb qt-commander_${VERSION}-${RELEASE}_${ARCH}.deb
