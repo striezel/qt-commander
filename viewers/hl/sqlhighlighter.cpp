8bbaf1f7cf5516a65e6e046818059b512c444278
Dirk Stolle <striezel-dev@web.de>
2026-06-09 20:20:46 +0200

    improve speed of SQL syntax highlighting
    With almost 400 keywords, the regular expression approach takes
    a lot of time. So instead of using one regular expression per
    keyword, the highlighter will now match several keywords in a
    single regular expression. Currently, up to 15 keywords are
    matched per regular expression. The speedup is noticeable. It's
    approx. nine times as fast now. (Speedup may be different on
    other machines and with different SQL files.)

--- viewers/hl/sqlhighlighter.cpp
+++ viewers/hl/sqlhighlighter.cpp
@@ -422,9 +422,35 @@ SqlHighlighter::SqlHighlighter(const Theme& theme, QTextDocument* parent)
 
     HighlighterRule rule;
 
+    constexpr unsigned int words_per_regex = 15;
+    unsigned int word_count = 0;
+    QString words;
+
     for (const std::string& keyword : keywords)
     {
-        rule.pattern = QRegularExpression(QString::fromStdString("\\b" + keyword + "\\b"), QRegularExpression::CaseInsensitiveOption);
+        if (words.isEmpty())
+        {
+            words = QString::fromStdString(keyword);
+        }
+        else
+        {
+            words += "|" + keyword;
+        }
+        ++word_count;
+        if (word_count >= words_per_regex)
+        {
+            rule.pattern = QRegularExpression("\\b(" + words + ")\\b", QRegularExpression::CaseInsensitiveOption);
+            rule.format = keywordFormat;
+            rules.append(rule);
+
+            words.clear();
+            word_count = 0;
+        }
+    }
+
+    if (word_count > 0)
+    {
+        rule.pattern = QRegularExpression("\\b(" + words + ")\\b", QRegularExpression::CaseInsensitiveOption);
         rule.format = keywordFormat;
         rules.append(rule);
     }
